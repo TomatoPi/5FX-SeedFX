b5a08e9489bc240ce440f1fad1372af971decde4
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-02-19 04:27:07 +0100

    :ambulance: FIXED

--- src/Chorus.hpp
+++ src/Chorus.hpp
@@ -42,43 +42,62 @@ namespace sfx
     constexpr const size_t GrainMaxSize = 1L << 15;
     constexpr const size_t CloudMaxSize = 8;
 
+    float delays[CloudMaxSize] = { 5.f, 7.f, 11.f, 13.f, 17.f, 19.f, 23.f, 29.f };
+    float frequencies[CloudMaxSize] = { 25.f, 10.f };
+    float depths[CloudMaxSize] = { 0.015f, 0.021f };
+    float grain_size_ms = 100.f;
+    float dry_gain = -3db;
+    float wet_gain = -3db;
+    float feedback_gain = 0.f;
+    size_t cloud_size = 2;
+
     sfx::Buffer<BufferSize> DSY_SDRAM_BSS _buffer;
     float DSY_SDRAM_BSS _window[GrainMaxSize];
 
-    struct Granulator
-    {
-      struct Grain
-      {
-        LFO lfo;
-        size_t time;
-        float read_h;
+    LFO _lfos[CloudMaxSize];
+    size_t _anchors[CloudMaxSize];
+    size_t _times[CloudMaxSize * 2];
+    float _read_hs[CloudMaxSize * 2];
 
-        void Init(float sr, float freq, float phase, const Granulator& g);
-        float Read(const Granulator& g);
-      };
-
-      void Init(float sr, float freq, float delay, float grain);
-      void SetDepth(float d);
-      float Read();
-
-      Grain ga, gb;
-      size_t anchor, grain_length;
-      float depth;
-
-    } _granulators[CloudMaxSize];
-
-    struct Params
-    {
-      size_t cloud_size;
-      float dry;
-      float wet;
-      float feedback;
-
-    } _params;
+    size_t _grain_length;
 
     void Init(float sr);
-    float Read();
     float Process(float x);
+
+    // struct Granulator
+    // {
+    //   struct Grain
+    //   {
+    //     LFO lfo;
+    //     size_t time;
+    //     float read_h;
+
+    //     void Init(float sr, float freq, float phase, const Granulator& g);
+    //     float Read(const Granulator& g);
+    //   };
+
+    //   void Init(float sr, float freq, float delay, float grain);
+    //   void SetDepth(float d);
+    //   float Read();
+
+    //   Grain ga, gb;
+    //   size_t anchor, grain_length;
+    //   float depth;
+
+    // } _granulators[CloudMaxSize];
+
+    // struct Params
+    // {
+    //   size_t cloud_size;
+    //   float dry;
+    //   float wet;
+    //   float feedback;
+
+    // } _params;
+
+    // void Init(float sr);
+    // float Read();
+    // float Process(float x);
   }
 }
 
@@ -86,6 +105,66 @@ namespace sfx
 
 /////////////////////////////////////////////////////////////////////////
 
+namespace sfx
+{
+  namespace Chorus
+  {
+    namespace details
+    {
+      float accumulateVoices()
+      {
+        float sample = 0.f;
+        for (size_t i = 0; i < cloud_size; ++i) {
+          for (size_t c = 0; c < 2; ++c) {
+            size_t time = _times[2 * i + c];
+            float read_h = _read_hs[2 * i + c];
+            sample += _window[time] * _buffer.Read(read_h);
+            time = (time + 1) % _grain_length;
+            if (0 == time) {
+              read_h = static_cast<float>(_anchors[i]);
+            } else {
+              read_h += 1.f;
+            }
+            _times[2 * i + c] = time;
+            _read_hs[2 * i + c] = fmod(read_h + depths[i] * _lfos[i].Process(), BufferSize);
+          }
+          _anchors[i] = (_anchors[i] + 1) & (BufferSize - 1);
+        }
+        return sample / (float)cloud_size;
+      }
+    }
+
+    void Init(float sr)
+    {
+      _buffer.Init();
+      _grain_length = grain_size_ms * 0.001f * sr;
+
+      for (size_t i = 0; i < cloud_size; ++i) {
+        _lfos[i].Init(sr);
+        _lfos[i].SetFreq(frequencies[i]);
+
+        _anchors[i] = BufferSize - (delays[i] * 0.001f * sr);
+
+        for (size_t c = 0; c < 2; ++c) {
+          _times[2 * i + c] = 0.5f * c * _grain_length;
+          _read_hs[2 * i + c] = static_cast<size_t>(_anchors[i]);
+        }
+      }
+      for (size_t i = 0; i < _grain_length; ++i) {
+        _window[i] = sin((M_PI * i) / (float)(_grain_length));
+      }
+    }
+    float Process(float x)
+    {
+      float wet_sample = details::accumulateVoices();
+      float output = dry_gain * x + wet_gain * wet_sample;
+      _buffer.Write(x + feedback_gain * wet_sample);
+      return output;
+    }
+  }
+}
+
+#if 0
 namespace sfx
 {
   namespace Chorus
@@ -183,3 +262,4 @@ namespace sfx
     }
   }
 }
+#endif
